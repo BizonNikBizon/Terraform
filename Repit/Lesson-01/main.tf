76284553a35d70b0c1f2207ddc9c0cb1679e85ef
bizonnikbizon <bizonnikbizon@gmail.com>
2023-08-15 16:24:24 +0300

    Повтор Урок-1

--- Repit/Lesson-01/main.tf
+++ Repit/Lesson-01/main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
-  region = us-west-1
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "My_Linux" {
+  count         = 3
+  ami           = "ami-0c4c4bd6cf0c5fe52"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Linux"
+  }
+}
+
+
+resource "aws_instance" "My_Ubuntu" {
+  ami           = "ami-04e601abe3e1a910f"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Ubuntu"
+  }
 }
